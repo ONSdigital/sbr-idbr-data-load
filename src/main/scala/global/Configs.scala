5a67d5024e08018fe69eed9414b0443913bb4156
ONS-Anthony <ChiuA@MacBook-Pro.local>
2018-04-11 11:18:47 +0100

    config cleanup

--- src/main/scala/global/Configs.scala
+++ src/main/scala/global/Configs.scala
@@ -15,6 +15,12 @@ object Configs{
 
   val config: Config = ConfigFactory.load()
 
+  val entHfile = "files.enterprise.hfile"
+  val entLinks = "files.links.ent.hfile"
+  val louHfile = "files.lou.hfile"
+  val louLinks = "files.links.lou.hfile"
+  val dataDir = "src/main/resources/data"
+
   val conf: Configuration = HBaseConfiguration.create()
   Try{config.getString("hadoop.security.authentication")}.map(conf.addResource).getOrElse(conf.set("hadoop.security.authentication","kerberos"))
   Try{config.getString("hbase.security.authentication")}.map(conf.addResource).getOrElse(conf.set("hbase.security.authentication","kerberos"))
@@ -28,22 +34,22 @@ object Configs{
   Try{config.getString("hbase.table.links.name")}.map(conf.set("hbase.table.links.name",_)).getOrElse(conf.set("hbase.table.links.name","LINKS"))
   Try{config.getString("hbase.table.links.column.family")}.map(conf.set("hbase.table.links.column.family",_)).getOrElse(conf.set("hbase.table.links.column.family","l"))
   Try{config.getString("hbase.table.links.namespace")}.map(conf.set("hbase.table.links.namespace",_)).getOrElse(conf.set("hbase.table.links.namespace","ons"))
-  Try{config.getString("files.links.ent.hfile")}.map(conf.set("files.links.ent.hfile",_)).getOrElse(conf.set("files.links.ent.hfile","src/main/resources/data/links/ent/hfile"))
-  Try{config.getString("files.links.lou.hfile")}.map(conf.set("files.links.lou.hfile",_)).getOrElse(conf.set("files.links.lou.hfile","src/main/resources/data/links/lou/hfile"))
+  Try{config.getString(entLinks)}.map(conf.set(entLinks,_)).getOrElse(conf.set(entLinks,s"$dataDir/links/ent/hfile"))
+  Try{config.getString(louLinks)}.map(conf.set(louLinks,_)).getOrElse(conf.set(louLinks,s"$dataDir/links/lou/hfile"))
 
   Try{config.getString("hbase.table.enterprise.name")}.map(conf.set("hbase.table.enterprise.name",_)).getOrElse(conf.set("hbase.table.enterprise.name","ENT"))
   Try{config.getString("hbase.table.enterprise.column.family")}.map(conf.set("hbase.table.enterprise.column.family",_)).getOrElse(conf.set("hbase.table.enterprise.column.family","d"))
   Try{config.getString("hbase.table.enterprise.namespace")}.map(conf.set("hbase.table.enterprise.namespace",_)).getOrElse(conf.set("hbase.table.enterprise.namespace","ons"))
-  Try{config.getString("files.enterprise.hfile")}.map(conf.set("files.enterprise.hfile",_)).getOrElse(conf.set("files.enterprise.hfile","src/main/resources/data/enterprise/hfile"))
+  Try{config.getString(entHfile)}.map(conf.set(entHfile,_)).getOrElse(conf.set(entHfile,s"$dataDir/enterprise/hfile"))
 
   Try{config.getString("hbase.table.lou.name")}.map(conf.set("hbase.table.lou.name",_)).getOrElse(conf.set("hbase.table.lou.name","LOU"))
   Try{config.getString("hbase.table.lou.column.family")}.map(conf.set("hbase.table.lou.column.family",_)).getOrElse(conf.set("hbase.table.lou.column.family","d"))
   Try{config.getString("hbase.table.lou.namespace")}.map(conf.set("hbase.table.lou.namespace",_)).getOrElse(conf.set("hbase.table.lou.namespace","ons"))
-  Try{config.getString("files.lou.hfile")}.map(conf.set("files.lou.hfile",_)).getOrElse(conf.set("files.lou.hfile","src/main/resources/data/lou/hfile"))
+  Try{config.getString(louHfile)}.map(conf.set(louHfile,_)).getOrElse(conf.set(louHfile,s"$dataDir/lou/hfile"))
 
-  Try{config.getString("files.parquet")}.map(conf.set("files.parquet",_)).getOrElse(conf.set("files.parquet","src/main/resources/data/test.parquet"))
-  Try{config.getString("files.lou.csv")}.map(conf.set("files.lou.csv",_)).getOrElse(conf.set("files.lou.csv","src/main/resources/data/sampleLocal.csv"))
-  Try{config.getString("files.ent.csv")}.map(conf.set("files.ent.csv",_)).getOrElse(conf.set("files.ent.csv","src/main/resources/data/idbr.csv"))
+  Try{config.getString("files.parquet")}.map(conf.set("files.parquet",_)).getOrElse(conf.set("files.parquet",s"$dataDir/test.parquet"))
+  Try{config.getString("files.lou.csv")}.map(conf.set("files.lou.csv",_)).getOrElse(conf.set("files.lou.csv",s"$dataDir/sampleLocal.csv"))
+  Try{config.getString("files.ent.csv")}.map(conf.set("files.ent.csv",_)).getOrElse(conf.set("files.ent.csv",s"$dataDir/idbr.csv"))
 
   Try{config.getString("enterprise.data.timeperiod")}.map(conf.set("enterprise.data.timeperiod",_)).getOrElse(conf.set("enterprise.data.timeperiod","201802"))
   Try{config.getString("spark.deployment.mode")}.map(conf.set("spark.deployment.mode",_)).getOrElse(conf.set("spark.deployment.mode","local"))
@@ -52,18 +58,18 @@ object Configs{
   lazy val HBASE_LINKS_TABLE_NAMESPACE = conf.getStrings("hbase.table.links.namespace").head
   lazy val HBASE_LINKS_COLUMN_FAMILY = conf.getStrings("hbase.table.links.column.family").head
 
-  lazy val PATH_TO_LINKS_ENT_HFILE =  conf.getStrings("files.links.ent.hfile").head
-  lazy val PATH_TO_LINKS_LOU_HFILE =  conf.getStrings("files.links.lou.hfile").head
+  lazy val PATH_TO_LINKS_ENT_HFILE =  conf.getStrings(entLinks).head
+  lazy val PATH_TO_LINKS_LOU_HFILE =  conf.getStrings(louLinks).head
 
   lazy val HBASE_ENTERPRISE_TABLE_NAME = conf.getStrings("hbase.table.enterprise.name").head
   lazy val HBASE_ENTERPRISE_TABLE_NAMESPACE = conf.getStrings("hbase.table.enterprise.namespace").head
   lazy val HBASE_ENTERPRISE_COLUMN_FAMILY = conf.getStrings("hbase.table.enterprise.column.family").head
-  lazy val PATH_TO_ENT_HFILE =  conf.getStrings("files.enterprise.hfile").head
+  lazy val PATH_TO_ENT_HFILE =  conf.getStrings(entHfile).head
 
   lazy val HBASE_LOU_TABLE_NAME = conf.getStrings("hbase.table.lou.name").head
   lazy val HBASE_LOU_TABLE_NAMESPACE = conf.getStrings("hbase.table.lou.namespace").head
   lazy val HBASE_LOU_COLUMN_FAMILY = conf.getStrings("hbase.table.lou.column.family").head
-  lazy val PATH_TO_LOU_HFILE =  conf.getStrings("files.lou.hfile").head
+  lazy val PATH_TO_LOU_HFILE =  conf.getStrings(louHfile).head
 
   lazy val PATH_TO_PARQUET = conf.getStrings("files.parquet").head
   lazy val PATH_TO_LOU_CSV  = conf.getStrings("files.lou.csv").head
@@ -75,16 +81,16 @@ object Configs{
 
     Try(args(0)).map(conf.set("hbase.table.links.name", _)).getOrElse(Unit)
     Try(args(1)).map(conf.set("hbase.table.links.namespace", _)).getOrElse(Unit)
-    Try(args(2)).map(conf.set("files.links.ent.hfile", _)).getOrElse(Unit)
-    Try(args(3)).map(conf.set("files.links.lou.hfile", _)).getOrElse(Unit)
+    Try(args(2)).map(conf.set(entLinks, _)).getOrElse(Unit)
+    Try(args(3)).map(conf.set(louLinks, _)).getOrElse(Unit)
 
     Try(args(4)).map(conf.set("hbase.table.enterprise.name", _)).getOrElse(Unit)
     Try(args(5)).map(conf.set("hbase.table.enterprise.namespace", _)).getOrElse(Unit)
-    Try(args(6)).map(conf.set("files.enterprise.hfile", _)).getOrElse(Unit)
+    Try(args(6)).map(conf.set(entHfile, _)).getOrElse(Unit)
 
     Try(args(7)).map(conf.set("hbase.table.lou.name", _)).getOrElse(Unit)
     Try(args(8)).map(conf.set("hbase.table.lou.namespace", _)).getOrElse(Unit)
-    Try(args(9)).map(conf.set("files.lou.hfile", _)).getOrElse(Unit)
+    Try(args(9)).map(conf.set(louHfile, _)).getOrElse(Unit)
 
     Try(args(10)).map(conf.set("files.parquet", _)).getOrElse(Unit)
     Try(args(11)).map(conf.set("files.lou.csv", _)).getOrElse(Unit)
