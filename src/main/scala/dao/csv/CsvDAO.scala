a03dbb2fa603614d240755e1f1d4c0bb1af2d707
ONS-Anthony <ChiuA@MacBook-Pro.local>
2018-04-19 14:47:00 +0100

    coalesce dataframe

--- src/main/scala/dao/csv/CsvDAO.scala
+++ src/main/scala/dao/csv/CsvDAO.scala
@@ -17,7 +17,7 @@ object CsvDAO extends WithConversionHelper with HFileWriter with  DataFrameHelpe
     val df = louDF.select("ern","sic07","employees")
 
     val sicRDD = getSection(df, divisions)
-    val entRDD = entDF.join(sicRDD, "ern").dropDuplicates("ern","sic07").rdd.map(row => toRecord(row, "ent")).cache
+    val entRDD = entDF.join(sicRDD, "ern").dropDuplicates("ern","sic07").coalesce(df.rdd.getNumPartitions).rdd.map(row => toRecord(row, "ent")).cache
     val louRDD = spark.read.option("header", "true").csv(PATH_TO_LOU_CSV).rdd.map(row => toRecord(row, "lou")).cache
 
     toHFile(entRDD, PATH_TO_ENT_HFILE)
